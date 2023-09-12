f0a07372b7ddf84f123794d5d6eaf036d2c62580
VedantTheGreat <vedantswamy2004@gmail.com>
2023-09-12 23:44:37 +0530

    Made changes

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,11 +7,11 @@ class FizzBuzz {
         int i=1;
         while (i < 100) {
             i ++;
-            dofizzbuz(i);
+            doFizzBuzz(i);
         }
     }
 
-    private static int dofizzbuz( int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
